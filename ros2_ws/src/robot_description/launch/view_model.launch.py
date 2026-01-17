5bc743a31d19e04504452624c0106d2cea9dc4c6
Mohammad-shaaban1 <mohammadchaaban2003@gmail.com>
2026-01-17 18:21:55 +0200

    Add description_urdf files to ros2_ws

---
+++ ros2_ws/src/robot_description/launch/view_model.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+import xacro
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('robot_description')
+    xacro_file = os.path.join(pkg_share, 'urdf', 'green_guardian.urdf.xacro')
+    rviz_file = os.path.join(pkg_share, 'rviz', 'model.rviz')
+
+    robot_description = xacro.process_file(xacro_file).toxml()
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    rviz2 = Node(
+    package='rviz2',
+    executable='rviz2',
+    output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        rviz2
+    ])
